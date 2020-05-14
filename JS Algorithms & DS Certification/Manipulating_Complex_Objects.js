418c8fd4e8d23c6373de49c72729bcb156bf3441
Abhisek Juneja <abhisekjuneja@protonmail.com>
2020-05-14 11:17:19 +0530

    Completed 90% of 'JS Algorithms & DS Certification - Basic JavaScript'

---
+++ JS Algorithms & DS Certification/Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, 
+  // Add a record here
+  {
+    "artist": "Ed Sheeran",
+    "title": "Divide",
+    "release_year": 2019,
+    "formats": [
+      "Vinyl",
+      "Digital"
+    ],
+    "gold": true
+  }
+];
